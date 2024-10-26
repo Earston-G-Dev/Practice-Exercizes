dea895d56c4a1140c96d9d01dc85ffdb3f8bcba2
Earston-G-Dev <markus.williams95.dev@gmail.com>
2024-10-26 12:24:27 -0700

    made pages for notes on subjects im working on

---
+++ Practice/practice.js
@@ -0,0 +1,38 @@
+'use strict'
+
+
+
+
+
+// function add(x) => {
+//     return x+x;
+// }
+// console.log(add);
+// This function represents expressions vs declarations and arrow functions.
+// The bottom is a correct form the above is my first attempt without a legend, only issue is i made a = sign and arrow. 
+
+
+function add(x) {
+    return x+x;
+}
+ 
+console.log(add(10));
+
+
+
+
+
+const addition= function(y){
+return y+y;
+};
+
+console.log(addition(7));
+
+
+// ARRAYS
+const team =[Bry, Zay, Shawn, Knuckle, Greg, Derrel, Antonio, Jedidiah];
+
+const numberEx = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//Task-  Create an array of numbers then create a .map() to create a new array that contains square numbers.
+
+
