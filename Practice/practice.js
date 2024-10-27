2a3f455b00aac72badb541b6370b1ba171115e75
Earston-G-Dev <markus.williams95.dev@gmail.com>
2024-10-27 13:17:09 -0700

    starting with objects

--- Practice/practice.js
+++ Practice/practice.js
@@ -30,7 +30,7 @@ console.log(addition(7));
 
 
 // ARRAYS
-const team = [Bry, Zay, Shawn, Knuckle, Greg, Derrel, Antonio, Jedidiah];
+const team = ['Jaelin','Mystic','Shawn','Zay','Bry', 'Echo', 'Greg', 'Jedidiah', 'Patrick', 'Retro' ];
 
 const numberEx = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //Task-  Create an array of numbers then create a .map() to create a new array that contains square numbers.
@@ -49,4 +49,19 @@ const squares = numberEx.map(function(x) {
 });
 
 console.log(squares); // Output: [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+
+
+//Now lets try doing it by every element x2
+
+const double = numberEx.map(function(y) {
+    return y * 2;
+});
+
+console.log(double);
+
+//Can also be done as a arrow function
+const triple = numberEx.map(v => v*3);
+
+console.log(triple);
 
