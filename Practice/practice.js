3e0f462347667b266cd5bd1f0e3e42149ee5fc27
Earston-G-Dev <markus.williams95.dev@gmail.com>
2024-10-27 05:13:37 -0700

    Updated formula for Arrays

--- Practice/practice.js
+++ Practice/practice.js
@@ -30,13 +30,23 @@ console.log(addition(7));
 
 
 // ARRAYS
-const team =[Bry, Zay, Shawn, Knuckle, Greg, Derrel, Antonio, Jedidiah];
+const team = [Bry, Zay, Shawn, Knuckle, Greg, Derrel, Antonio, Jedidiah];
 
 const numberEx = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //Task-  Create an array of numbers then create a .map() to create a new array that contains square numbers.
 
-const square= function(x){
-    numbersEx.forEach(){
-        return numbersEx
-    }
-}
+// const square= function(x){
+//     numbersEx.forEach(){
+//         return numbersEx
+//     }
+// }
+
+
+//Solution
+// Task: Create an array that contains the squares of each number
+const squares = numberEx.map(function(x) {
+    return x * x;
+});
+
+console.log(squares); // Output: [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
